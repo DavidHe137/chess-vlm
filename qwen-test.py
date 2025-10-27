bf8693e60b3837c61379bddd9540d161c6fe409f
Kevin Rojas <kevinrojas99@gmail.com>
2025-10-27 14:31:40 -0400

    Added qwen test file

---
+++ qwen-test.py
@@ -0,0 +1,202 @@
+from huggingface_hub import notebook_login
+
+system_message = """
+You are a helpful assistant that can answer questions about the image.
+"""
+def format_data(sample):
+    return {
+      "images": [sample["image"]],
+      "messages": [
+
+          {
+              "role": "system",
+              "content": [
+                  {
+                      "type": "text",
+                      "text": system_message
+                  }
+              ],
+          },
+          {
+              "role": "user",
+              "content": [
+                  {
+                      "type": "image",
+                      "image": sample["image"],
+                  },
+                  {
+                      "type": "text",
+                      "text": sample['query'],
+                  }
+              ],
+          },
+          {
+              "role": "assistant",
+              "content": [
+                  {
+                      "type": "text",
+                      "text": sample["label"][0]
+                  }
+              ],
+          },
+      ]
+      }
+
+from datasets import load_dataset
+
+dataset_id = "HuggingFaceM4/ChartQA"
+train_dataset, eval_dataset, test_dataset = load_dataset(dataset_id, split=['train[:10%]', 'val[:10%]', 'test[:10%]'])
+
+train_dataset = [format_data(sample) for sample in train_dataset]
+eval_dataset = [format_data(sample) for sample in eval_dataset]
+test_dataset = [format_data(sample) for sample in test_dataset]
+
+
+import torch
+from transformers import Qwen2VLForConditionalGeneration, Qwen2VLProcessor
+
+model_id = "Qwen/Qwen2-VL-7B-Instruct"
+
+device = "cuda"
+
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    model_id,
+    device_map=device,
+    torch_dtype=torch.bfloat16,
+)
+
+processor = Qwen2VLProcessor.from_pretrained(model_id)
+
+
+from qwen_vl_utils import process_vision_info
+
+def generate_text_from_sample(model, processor, sample, max_new_tokens=1024, device="cuda"):
+    # Prepare the text input by applying the chat template
+    text_input = processor.apply_chat_template(
+        sample['messages'][1:2],  # Use the sample without the system message
+        tokenize=False,
+        add_generation_prompt=True
+    )
+
+    # Process the visual input from the sample
+    image_inputs, _ = process_vision_info(sample['messages'])
+
+    # Prepare the inputs for the model
+    model_inputs = processor(
+        text=[text_input],
+        images=image_inputs,
+        return_tensors="pt",
+    ).to(device)  # Move inputs to the specified device
+
+    # Generate text with the model
+    generated_ids = model.generate(**model_inputs, max_new_tokens=max_new_tokens)
+
+    # Trim the generated ids to remove the input ids
+    trimmed_generated_ids = [
+        out_ids[len(in_ids):] for in_ids, out_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    # Decode the output text
+    output_text = processor.batch_decode(
+        trimmed_generated_ids,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=False
+    )
+
+    return output_text[0]  # Return the first decoded output text
+
+
+# Example of how to call the method with sample:
+output = generate_text_from_sample(model, processor, train_dataset[0])
+output
+
+from transformers import BitsAndBytesConfig
+
+# BitsAndBytesConfig int-4 config
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+# Load model and tokenizer
+model = Qwen2VLForConditionalGeneration.from_pretrained(
+    model_id,
+    device_map=device,
+    torch_dtype=torch.bfloat16,
+    quantization_config=bnb_config
+)
+processor = Qwen2VLProcessor.from_pretrained(model_id)
+
+
+from peft import LoraConfig, get_peft_model
+
+# Configure LoRA
+peft_config = LoraConfig(
+    lora_alpha=16,
+    lora_dropout=0.05,
+    r=8,
+    bias="none",
+    target_modules=["q_proj", "v_proj"],
+    task_type="CAUSAL_LM",
+)
+
+# Apply PEFT model adaptation
+peft_model = get_peft_model(model, peft_config)
+
+# Print trainable parameters
+peft_model.print_trainable_parameters()
+
+from trl import SFTConfig
+
+# Configure training arguments
+training_args = SFTConfig(
+    output_dir="qwen2-7b-instruct-trl-sft-ChartQA",  # Directory to save the model
+    num_train_epochs=3,  # Number of training epochs
+    per_device_train_batch_size=4,  # Batch size for training
+    per_device_eval_batch_size=4,  # Batch size for evaluation
+    gradient_accumulation_steps=8,  # Steps to accumulate gradients
+    gradient_checkpointing_kwargs={"use_reentrant": False},  # Options for gradient checkpointing
+    max_length=None,
+    # Optimizer and scheduler settings
+    optim="adamw_torch_fused",  # Optimizer type
+    learning_rate=2e-4,  # Learning rate for training
+    # Logging and evaluation
+    logging_steps=10,  # Steps interval for logging
+    eval_steps=10,  # Steps interval for evaluation
+    eval_strategy="steps",  # Strategy for evaluation
+    save_strategy="steps",  # Strategy for saving the model
+    save_steps=20,  # Steps interval for saving
+    # Mixed precision and gradient settings
+    bf16=True,  # Use bfloat16 precision
+    max_grad_norm=0.3,  # Maximum norm for gradient clipping
+    warmup_ratio=0.03,  # Ratio of total steps for warmup
+    # Hub and reporting
+    push_to_hub=True,  # Whether to push model to Hugging Face Hub
+    report_to="trackio",  # Reporting tool for tracking metrics
+)
+
+import trackio
+
+trackio.init(
+    project="qwen2-7b-instruct-trl-sft-ChartQA",
+    name="qwen2-7b-instruct-trl-sft-ChartQA",
+    config=training_args,
+    space_id=training_args.output_dir + "-trackio"
+)
+
+from trl import SFTTrainer
+
+trainer = SFTTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    peft_config=peft_config,
+    processing_class=processor,
+)
+
+trainer.train()
+
+trainer.save_model(training_args.output_dir)
