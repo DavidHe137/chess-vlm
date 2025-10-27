6e83fe43205b1f69bba29301edcfb2794fbc7dc7
KevinRojas1499 <kevinrojas99@gmail.com>
2025-10-27 16:14:06 -0400

    Refactored sft

--- qwen-test.py
+++ qwen-test.py
@@ -1,3 +1,4 @@
+import click
 from huggingface_hub import notebook_login
 from datasets import load_dataset
 from transformers import BitsAndBytesConfig
@@ -10,93 +11,48 @@ import trackio
 from trl import SFTTrainer
 import pdb
 
-system_message = """
+
+SYSTEM_MESSAGE = """
 You are a helpful assistant that can answer questions about the image.
 """
 def format_data(sample):
     return {
-      "images": [sample["image"]],
-      "messages": [
+    "images": [sample["image"]],
+    "messages": [
 
-          {
-              "role": "system",
-              "content": [
-                  {
-                      "type": "text",
-                      "text": system_message
-                  }
-              ],
-          },
-          {
-              "role": "user",
-              "content": [
-                  {
-                      "type": "image",
-                      "image": sample["image"],
-                  },
-                  {
-                      "type": "text",
-                      "text": sample['query'],
-                  }
-              ],
-          },
-          {
-              "role": "assistant",
-              "content": [
-                  {
-                      "type": "text",
-                      "text": sample["label"][0]
-                  }
-              ],
-          },
-      ]
-      }
-
-dataset_id = "HuggingFaceM4/ChartQA"
-train_dataset, eval_dataset, test_dataset = load_dataset(dataset_id, split=['train[:10%]', 'val[:10%]', 'test[:10%]'])
-
-train_dataset = [format_data(sample) for sample in train_dataset]
-eval_dataset = [format_data(sample) for sample in eval_dataset]
-st_dataset = [format_data(sample) for sample in test_dataset]
-
-
-print("loading model")
-
-model_id = "Qwen/Qwen2-VL-7B-Instruct"
-
-device = "cuda"
-
-# Load model and tokenizer
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-model = Qwen2VLForConditionalGeneration.from_pretrained(
-    model_id,
-    device_map="auto",
-    torch_dtype=torch.bfloat16,
-    quantization_config=bnb_config
-)
-processor = Qwen2VLProcessor.from_pretrained(model_id)
-
-# BitsAndBytesConfig int-4 config
-# Configure LoRA
-peft_config = LoraConfig(
-    lora_alpha=16,
-    lora_dropout=0.05,
-    r=8,
-    bias="none",
-    target_modules=["q_proj", "v_proj"],
-    task_type="CAUSAL_LM",
-)
-
-# Apply PEFT model adaptation
-peft_model = get_peft_model(model, peft_config)
-
-# Print trainable parameters
-peft_model.print_trainable_parameters()
+        {
+            "role": "system",
+            "content": [
+                {
+                    "type": "text",
+                    "text": SYSTEM_MESSAGE
+                }
+            ],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": sample["image"],
+                },
+                {
+                    "type": "text",
+                    "text": sample['query'],
+                }
+            ],
+        },
+        {
+            "role": "assistant",
+            "content": [
+                {
+                    "type": "text",
+                    "text": sample["label"][0]
+                }
+            ],
+        },
+    ]
+    }
 
 
 def generate_text_from_sample(model, processor, sample, max_new_tokens=1024, device="cuda"):
@@ -135,53 +91,103 @@ def generate_text_from_sample(model, processor, sample, max_new_tokens=1024, dev
     return output_text[0]  # Return the first decoded output text
 
 
-# Example of how to call the method with sample:
-output = generate_text_from_sample(model, processor, train_dataset[0])
-print(output)
+@click.command()
+@click.option("--dataset_id", default="HuggingFaceM4/ChartQA", type=str, required=True)
+@click.option("--model_id", default="Qwen/Qwen2-VL-7B-Instruct", type=str, required=True)
+@click.option("--output_dir", default="qwen2-7b-instruct-trl-sft-ChartQA", type=str, required=True)
+def sft(dataset_id, model_id, output_dir):
+    """
+    Train a Qwen2 VL model on a dataset.
+    """
 
-# Configure training arguments
-training_args = SFTConfig(
-    output_dir="qwen2-7b-instruct-trl-sft-ChartQA",  # Directory to save the model
-    num_train_epochs=3,  # Number of training epochs
-    per_device_train_batch_size=4,  # Batch size for training
-    per_device_eval_batch_size=4,  # Batch size for evaluation
-    gradient_accumulation_steps=8,  # Steps to accumulate gradients
-    gradient_checkpointing_kwargs={"use_reentrant": False},  # Options for gradient checkpointing
-    max_length=None,
-    # Optimizer and scheduler settings
-    optim="adamw_torch_fused",  # Optimizer type
-    learning_rate=2e-4,  # Learning rate for training
-    # Logging and evaluation
-    logging_steps=10,  # Steps interval for logging
-    eval_steps=10,  # Steps interval for evaluation
-    eval_strategy="steps",  # Strategy for evaluation
-    save_strategy="steps",  # Strategy for saving the model
-    save_steps=20,  # Steps interval for saving
-    # Mixed precision and gradient settings
-    bf16=True,  # Use bfloat16 precision
-    max_grad_norm=0.3,  # Maximum norm for gradient clipping
-    warmup_ratio=0.03,  # Ratio of total steps for warmup
-    # Hub and reporting
-    push_to_hub=True,  # Whether to push model to Hugging Face Hub
-    report_to="trackio",  # Reporting tool for tracking metrics
-)
+    # Load dataset
+    click.echo("loading dataset")
+    train_dataset, eval_dataset, test_dataset = load_dataset(dataset_id, split=['train[:10%]', 'val[:10%]', 'test[:10%]'])
 
-trackio.init(
-    project="qwen2-7b-instruct-trl-sft-ChartQA",
-    name="qwen2-7b-instruct-trl-sft-ChartQA",
-    config=training_args,
-    space_id=training_args.output_dir + "-trackio"
-)
+    train_dataset = [format_data(sample) for sample in train_dataset]
+    eval_dataset = [format_data(sample) for sample in eval_dataset]
+    st_dataset = [format_data(sample) for sample in test_dataset]
 
-trainer = SFTTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=eval_dataset,
-    peft_config=peft_config,
-    processing_class=processor,
-)
 
-trainer.train()
+    # Load model and tokenizer
+    click.echo("loading model")
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16
+    )
+    model = Qwen2VLForConditionalGeneration.from_pretrained(
+        model_id,
+        device_map="auto",
+        torch_dtype=torch.bfloat16,
+        quantization_config=bnb_config
+    )
+    processor = Qwen2VLProcessor.from_pretrained(model_id)
 
-trainer.save_model(training_args.output_dir)
+    # BitsAndBytesConfig int-4 config
+    # Configure LoRA
+    peft_config = LoraConfig(
+        lora_alpha=16,
+        lora_dropout=0.05,
+        r=8,
+        bias="none",
+        target_modules=["q_proj", "v_proj"],
+        task_type="CAUSAL_LM",
+    )
+
+    # Apply PEFT model adaptation
+    peft_model = get_peft_model(model, peft_config)
+
+    # Print trainable parameters
+    peft_model.print_trainable_parameters()
+
+    # Configure training arguments
+    training_args = SFTConfig(
+        output_dir="qwen2-7b-instruct-trl-sft-ChartQA",  # Directory to save the model
+        num_train_epochs=3,  # Number of training epochs
+        per_device_train_batch_size=4,  # Batch size for training
+        per_device_eval_batch_size=4,  # Batch size for evaluation
+        gradient_accumulation_steps=8,  # Steps to accumulate gradients
+        gradient_checkpointing_kwargs={"use_reentrant": False},  # Options for gradient checkpointing
+        max_length=None,
+        # Optimizer and scheduler settings
+        optim="adamw_torch_fused",  # Optimizer type
+        learning_rate=2e-4,  # Learning rate for training
+        # Logging and evaluation
+        logging_steps=10,  # Steps interval for logging
+        eval_steps=10,  # Steps interval for evaluation
+        eval_strategy="steps",  # Strategy for evaluation
+        save_strategy="steps",  # Strategy for saving the model
+        save_steps=20,  # Steps interval for saving
+        # Mixed precision and gradient settings
+        bf16=True,  # Use bfloat16 precision
+        max_grad_norm=0.3,  # Maximum norm for gradient clipping
+        warmup_ratio=0.03,  # Ratio of total steps for warmup
+        # Hub and reporting
+        push_to_hub=True,  # Whether to push model to Hugging Face Hub
+        report_to="trackio",  # Reporting tool for tracking metrics
+    )
+
+    trackio.init(
+        project="qwen2-7b-instruct-trl-sft-ChartQA",
+        name="qwen2-7b-instruct-trl-sft-ChartQA",
+        config=training_args,
+        space_id=training_args.output_dir + "-trackio"
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        peft_config=peft_config,
+        processing_class=processor,
+    )
+
+    trainer.train()
+
+    trainer.save_model(training_args.output_dir)
+
+if __name__ == "__main__":
+    sft()
